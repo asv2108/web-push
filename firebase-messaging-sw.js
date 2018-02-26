6ccc20e8893a1084cdc3d26ef5c1c169aab1772a
Alexey <asv2108@gmail.com>
2018-02-26 09:59:36 +0200

    add js local

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,19 @@
-importScripts('/__/firebasejs/4.10.1/firebase-app.js');
-importScripts('/__/firebasejs/4.10.1/firebase-messaging.js');
-
-
 var config = {
+    apiKey: "AIzaSyBaaV8jUe_6oHafyAHLGiN2uO-Tfi7bFcg",
+    authDomain: "vasilyev-e6d1e.firebaseapp.com",
+    databaseURL: "https://vasilyev-e6d1e.firebaseio.com",
+    projectId: "vasilyev-e6d1e",
+    storageBucket: "",
     messagingSenderId: "709625869743"
 };
 firebase.initializeApp(config);
 
-firebase.messaging();
+const messaging = firebase.messaging();
+//messaging.usePublicVapidKey("YM4BjG3wuACJ6yVW6dzSIB6ck5on5TO35JFDbORU3_g");
 
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
+messaging.requestPermission().then(function () {
+    console.log('Have permission');
+    return messaging.getToken();
+}).then(function (token) { console.log(token) }).catch(function (err) {
+    console.log(err);
 });
